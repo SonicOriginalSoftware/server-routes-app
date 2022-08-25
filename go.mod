452100d31d811f5854a30242055fb8a58d73a83a
Nathan Blair <me@nathanblair.rocks>
2022-08-25 01:42:30 -0400

    adding a workspace file
    and stubbing out a test file

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module routes/app
 
 go 1.19
+
+require (
+	server v0.0.0
+)
---
+++ handler_test.go
@@ -0,0 +1,7 @@
+package app_test
+
+import "testing"
+
+func TestHandler(t *testing.T) {
+	t.Fatalf("Not implemented yet!")
+}
